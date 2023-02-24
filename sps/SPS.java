6f48c776ff380d969e03eb88d5c3f7b79ea3f9e5
Electro-Corp <chinmaytanmay@outlook.com>
2023-02-23 18:34:29 -0700

    sps++

--- sps/SPS.java
+++ sps/SPS.java
@@ -1,4 +1,13 @@
 import java.util.ArrayList;
 class SPS{
-	public List<List<Note>> board = new ArrayList<List<Note>>;
+	//public List<List<Note>> board = new ArrayList<List<Note>>;
+	public Note[][] board = new Note[88][100];
+	public int[] keyVals = new int[88];
+	public int[] pitches = new int[88];
+	public SPS(){
+		
+	}
+	public void addNote(int key){
+		board[key][keyVals[key]++] = new Note(pitches[key],1);
+	}
 }
