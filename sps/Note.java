ac88b64e3975779dda2ceae7889854fabd27af3f
Electro-Corp <chinmaytanmay@outlook.com>
2023-02-23 18:27:41 -0700

    sps

---
+++ sps/Note.java
@@ -0,0 +1,8 @@
+class Note{
+	public double pitch, duration;
+	public Note(double pitch, duration){
+		this.pitch = pitch;
+		this.duration = duration;
+	}
+}
+
---
+++ sps/SPS.java
@@ -0,0 +1,4 @@
+import java.util.ArrayList;
+class SPS{
+	public List<List<Note>> board = new ArrayList<List<Note>>;
+}
