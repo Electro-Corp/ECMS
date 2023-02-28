bb17c870035487459b2d450c80b0e29ac8a5fb04
Androo <you@example.com>
2023-02-28 09:39:25 -0700

    fmfmfm,

--- src/main/java/net/badmidi/ecms/App.java
+++ src/main/java/net/badmidi/ecms/App.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import net.badmidi.ecms.sps.SPS;
-
 import java.io.*;
 import java.io.IOException;
 
