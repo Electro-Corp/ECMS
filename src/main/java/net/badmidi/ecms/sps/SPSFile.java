6269016d6a057647659ff4070ca74a5fafeecdd5
Chinmay Tiwari <26Chinmay.Tiwari@boiseschools.net>
2023-02-28 09:39:24 -0700

    delete some shit

--- src/main/java/net/badmidi/ecms/sps/SPSFile.java
+++
@@ -1,9 +0,0 @@
-package net.badmidi.ecms.sps;
-
-public class SPSFile {
-    public Note[][] board;
-    public int[] keyVals;
-    public int notes;
-    public int[] pitches;
-    public double testVal;
-}
